afd96d660077aa6eee07159b813d472632db4316
joe <joeboy229@gmail.com>
2021-10-19 14:55:44 +0800

    [chore] add an empty iOS project

---
+++ ios-project/Shared/ios_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_projectApp.swift
+//  Shared
+//
+//  Created by Joe Wu on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct ios_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

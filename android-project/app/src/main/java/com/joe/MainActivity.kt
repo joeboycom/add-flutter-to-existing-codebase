830c8467938fe1cb13d5b5847f35c4d1e222d820
joe <joeboy229@gmail.com>
2021-10-19 16:08:58 +0800

    [feat] add a button to open flutter activity

--- android-project/app/src/main/java/com/joe/MainActivity.kt
+++ android-project/app/src/main/java/com/joe/MainActivity.kt
@@ -2,10 +2,17 @@ package com.joe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btnOpenFlutterPage)
+        button.setOnClickListener {
+            startActivity(FlutterActivity.createDefaultIntent(this))
+        }
     }
 }
